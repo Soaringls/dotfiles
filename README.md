c0065ed1ce8f0427c5c33a0234292f580ddf94df
yongcong.wang <yongcong.wang@outlook.com>
2020-02-23 00:06:37 +0800

    rm zsh stuff

--- README.md
+++ README.md
@@ -16,11 +16,6 @@ bash <(curl -s https://raw.githubusercontent.com/yongcongwang/dotfiles/master/de
 bash <(curl -s https://raw.githubusercontent.com/yongcongwang/dotfiles/master/deploy.sh) tmux
 ```
 
-## zsh
-```bash
-bash <(curl -s https://raw.githubusercontent.com/yongcongwang/dotfiles/master/deploy.sh) zsh
-```
-
 ## all
 ```bash
 bash <(curl -s https://raw.githubusercontent.com/yongcongwang/dotfiles/master/deploy.sh) all
--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 
 # opts
 set -e  # err exit
-set -x  # cmd trace
+# set -x  # cmd trace
 # vars
 HOME="$(cd ~; pwd)"
 BASH_CONF_DIR="${HOME}/.bash"
@@ -54,10 +54,6 @@ function setup_tmux() {
   echo "Setup tmux start"
 }
 
-function setup_zsh() {
-  echo "Setup zsh start"
-}
-
 function main() {
   setup_environment
 
@@ -71,14 +67,10 @@ function main() {
     vim)
       setup_vim
       ;;
-    zsh)
-      setup_zsh
-      ;;
     all)
       setup_bash
       setup_tmux
       setup_vim
-      setup_zsh
       ;;
     *)
       echo "ERROR: Unknown paramemter: ${COMMAND}"
@@ -91,81 +83,3 @@ function main() {
 
 # start
 main
-
-# function setup_vim() {
-#   # install
-#   sudo apt install vim
-# 
-#   # setup config
-#   if [[ -d ${VIM_CONF_DIR} ]]; then
-#     rm -rf ${VIM_CONF_DIR}
-#   fi
-#   if [[ -f ${HOME}/.vimrc || -L ${HOME}/.vimrc ]]; then
-#     rm -f ${HOME}/.vimrc
-#   fi
-# 
-#   # download config
-#   git clone -b vim https://github.com/yongcongwang/software-config.git ~/.vim
-#   ln -s ${VIM_CONF_DIR}/vimrc ${HOME}/.vimrc
-# 
-#   # install plug manager
-#   curl -fLo ${VIM_CONF_DIR}/autoload/plug.vim --create-dirs \
-#     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-# 
-#   PLUG_DIR=${VIM_CONF_DIR}/plugged
-#   if [[ -d ${PLUG_DIR} ]]; then
-#     rm -rf ${PLUG_DIR}
-#   fi
-#   mkdir ${PLUG_DIR}
-# 
-#   # install plugins
-#   git clone https://github.com/altercation/vim-colors-solarized.git \
-#     ${PLUG_DIR}/vim-colors-solorized
-# 
-#   git clone https://github.com/plasticboy/vim-markdown.git \
-#     ${PLUG_DIR}/vim-markdown
-# 
-#   git clone https://github.com/octol/vim-cpp-enhanced-highlight.git \
-#     ${PLUG_DIR}/vim-cpp-enhanced-highlight
-#   
-#   git clone -b develop https://github.com/python-mode/python-mode.git \
-#     ${PLUG_DIR}/python-mode
-#   
-# }
-# 
-# function setup_zsh() {
-#   # install zsh
-#   sudo apt install zsh
-# 
-#   # setup config
-#   if [[ -d ${ZSH_CONF_DIR} ]]; then
-#     rm -rf ${ZSH_CONF_DIR}
-#   fi
-#   if [[ -f ${HOME}/.zshrc || -L ${HOME}/.zshrc ]]; then
-#     rm -f ${HOME}/.zshrc
-#   fi
-# 
-#   # download config
-#   git clone -b zsh https://github.com/yongcongwang/software-config.git \
-#     ${ZSH_CONF_DIR}
-#   ln -s ${ZSH_CONF_DIR}/zshrc ${HOME}/.zshrc
-# }
-# 
-# function setup_tmux() {
-#   # install tmux
-#   sudo apt install tmux
-# 
-#   # setup config
-#   if [[ -d ${TMUX_CONF_DIR} ]]; then
-#     rm -rf ${TMUX_CONF_DIR}
-#   fi
-#   if [[ -f ${HOME}/.tmux.conf || -L ${HOME}/.tmux.conf ]]; then
-#     rm -f ${HOME}/.tmux.conf
-#   fi
-# 
-#   # download config
-#   git clone -b tmux git@github.com:yongcongwang/software-config.git \
-#     ${TMUX_CONF_DIR}
-#   ln -s ${TMUX_CONF_DIR}/tmux.conf ${HOME}/.tmux.conf
-#   git clone https://github.com/tmux-plugins/tpm ${TMUX_CONF_DIR}/plugins/tpm
-# }
